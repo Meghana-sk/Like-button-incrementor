de3255c3967ff8f847a62479c81506b41bfd374d
Meghana <meghana.meghanask.sk@gmail.com>
2020-12-06 18:57:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
+var helloText = "Welcome";
+var color = "darkgray";
+
+// var username= prompt("enter username");
+var username = "Meghana";
+var welcomeMsg = "Welcome " + username;
+// var likeCounter = 0;
+
 export default function App() {
+  const [likeCounter, setLikeCounter] = useState(0);
+
+  function onClickHandler() {
+    var newLikeCounter = likeCounter + 1;
+    // console.log("clicked");
+    setLikeCounter(newLikeCounter);
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1 style={{ backgroundColor: color }}>{welcomeMsg}</h1>
+      <button onClick={onClickHandler}>Like me!</button>
+      {likeCounter}
     </div>
   );
 }
+
+/** Things to notice
+ * class => className
+ * background-color => backgroundColor
+ *
+ */
